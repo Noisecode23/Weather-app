797ce982064bb8b4c7db95bc834c95280ad37690
Noisecode23 <gamerstark210@gmail.com>
2024-01-22 22:37:49 +0530

    project completed added to repository

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ console.log("lets write javascript");
 
 const searchBox = document.querySelector("#searchInput")
 const searchBtn = document.querySelector(".search button")
+const weatherIcon = document.querySelector(".weather-icon")
 const apiKey = "56f53ea20c1b25732db4c87a9bffff23"
 function KtoC(K){
     return Math.floor(K - 273.15)
@@ -11,6 +12,10 @@ const Url =(city)=>
 
 async function getWeatherDetails(city) {
   const resp = await fetch(Url(city), {origin: "cors"});
+  if(resp.status == 404){
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  }
   const data = await resp.json();
 
   console.log(data);
@@ -20,8 +25,22 @@ async function getWeatherDetails(city) {
   document.querySelector(".humidity").innerHTML = data.main.humidity + "%"
   document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 
-
-
+//add images on condition
+if(data.weather[0].main == "Clear"){
+  weatherIcon.src = "weather-app-img/images/Clear.png"
+}
+else if(data.weather[0].main == "Clouds"){
+  weatherIcon.src = "weather-app-img/images/Clear.png"
+}
+else if(data.weather[0].main == "Rain"){
+  weatherIcon.src = "weather-app-img/images/rain.png"
+}
+else if(data.weather[0].main == "Drizzle"){
+  weatherIcon.src = "weather-app-img/images/dirizzle.png"
+}
+else if(data.weather[0].main == "Mist"){
+  weatherIcon.src = "weather-app-img/images/mist.png"
+}
 }
 searchBtn.addEventListener("click", (e)=>{
     e.preventDefault()
